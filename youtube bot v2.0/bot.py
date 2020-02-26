03b658701804543c897785356cfe33332fc4df8b
Dimitar Kostov <dimitar.d.kostov@hotmail.com>
2020-02-27 00:57:44 +0200

    fixes

--- youtube bot v2.0/bot.py
+++ youtube bot v2.0/bot.py
@@ -13,7 +13,7 @@ import keyboard
 
 def LoadUserAgents(uafile):
     uas = []
-    with open(uafile, 'rb') as uaf:
+    with open(uafile, 'r') as uaf:
         for ua in uaf.readlines():
             uas.append(ua.strip())
     random.shuffle(uas)
