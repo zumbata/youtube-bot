988fcf5882821a6fbe23f04d228f53060c462fcd
Dimitar Kostov <dimitar.d.kostov@hotmail.com>
2020-02-27 22:12:52 +0200

    update update

--- youtube bot v2.0/bot.py
+++ youtube bot v2.0/bot.py
@@ -93,4 +93,5 @@ devided_proxies = list(divide_chunks(all_proxies, int(len(all_proxies)
                        / num_threads)))
 for group_proxies in devided_proxies:
     some_thread = threading.Thread(target=func, args=(group_proxies, ))
+    some_thread.daemon = True
     some_thread.start()
