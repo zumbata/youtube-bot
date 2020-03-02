ca79cffe7a305317f5626cb446dbf72323fdb873
Hristiyan Ivanov <hrisivanov2@gmail.com>
2020-03-02 21:57:42 +0200

    Frontend (not final)

--- server/app/Http/Controllers/AppController.php
+++ server/app/Http/Controllers/AppController.php
@@ -6,24 +6,24 @@ use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
-    public function startBot(Request $request)
+    public function bots(Request $request)
     {
         $bot = ($request->input('bot') == "new") ? "new_bot" : "old_bot";
-        $encryped = base64_encode(json_encode([
-            "accounts" => $request->input('accounts'),
-            "comments" => $request->input('comments'),
-            "proxies" => $request->input('proxies'),
-            "keywords" => $request->input('keywords'),
-            "video" => $request->input('video'),
-            "threads" => $request->input('threads'),
-            "min_time" => $request->input('min_time'),
-            "max_time" => $request->input('max_time')
+        $encrypted = base64_encode(json_encode([
+            "accounts"  => $request->input('accounts'),
+            "comments"  => $request->input('comments'),
+            "proxies"   => $request->input('proxies'),
+            "keywords"  => $request->input('keywords'),
+            "video"     => $request->input('video'),
+            "threads"   => $request->input('threads'),
+            "min_time"  => $request->input('min_time'),
+            "max_time"  => $request->input('max_time')
         ]));
         // dont wait for it
         // shell_exec("python3 ../../{$bot}/bot.py {$encryped} > /var/log/custom_server_log/log.log 2>&1 &");
         //wait for it
-        shell_exec("python3 ../../{$bot}/bot.py {$encryped}");
-        return view('admin_start_bot', ['success' => true]);
+        shell_exec("python3 ../../{$bot}/bot.py {$encrypted}");
+        return view('pages.admin_start_bot', ['success' => true]);
     }
 
     public function login(Request $request)
@@ -33,16 +33,15 @@ class AppController extends Controller
         if($username == env('ADMIN_USERNAME') && $password == env('ADMIN_PASSWORD'))
         {
             $request->session()->put('isAdminLogged', true);
-            return redirect('/admin/startBot');
+            return redirect('/admin/bots');
         }
         else
-            return view('admin_login', ['success' => false]);
+            return view('pages.admin_login', ['success' => false]);
     }
     
     public function logout(Request $request)
     {
-        $request->session()->put('isAdminLogged', false);
         $request->session()->forget('isAdminLogged');
-        return redirect('/admin/login');
+        return redirect('/admin/login?logout=true');
     }
 }
---
+++ server/resources/views/includes/footer.blade.php
@@ -0,0 +1,1 @@
+<!-- <div id="copyright text-center">© Copyright 2013 Scotchy Scotch Scotch</div> -->
