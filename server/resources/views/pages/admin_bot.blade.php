29917567d19298be6d0e5835e41edba272c786bb
zumbata <zumbataweee@live.com>
2020-03-04 00:37:14 +0200

    fixes

--- server/resources/views/pages/admin_bot.blade.php
+++ server/resources/views/pages/admin_bot.blade.php
@@ -9,7 +9,7 @@
                 <div class="col-6">
                     <div class="form-group">
                         <label for="accounts">Google Accounts</label>
-                        <textarea class="form-control" id="accounts" rows="10" placeholder="Paste the accounts in the following format:
+                        <textarea class="form-control" name="accounts" id="accounts" rows="10" placeholder="Paste the accounts in the following format:
 email;password;recovery_email;comment
 email;password;recovery_email;comment
 email;password;recovery_email;comment
@@ -41,7 +41,7 @@ this is third example comment"></textarea>
                 <div class="col-6">
                     <div class="form-group">
                         <label for="proxies">Proxies</label>
-                        <textarea class="form-control" id="proxies" rows="10" placeholder="Paste the proxies in the following format:
+                        <textarea class="form-control" name="proxies" id="proxies" rows="10" placeholder="Paste the proxies in the following format:
 ip:port
 ip:port
 ip:port
@@ -77,13 +77,13 @@ this is third example keyword"></textarea>
                 </div>
                 <div class="col">
                     <div class="form-label-group">
-                        <input type="number" class="form-control" id="min-time" name="min-time" placeholder="">
+                        <input type="number" class="form-control" id="min-time" name="min_time" placeholder="">
                         <label for="min-time">MIN Watch Time</label>
                     </div>
                 </div>
                 <div class="col">
                     <div class="form-label-group">
-                        <input type="number" class="form-control" id="max-time" name="max-time" placeholder="">
+                        <input type="number" class="form-control" id="max-time" name="max_time" placeholder="">
                         <label for="max-time">MAX Watch Time</label>
                     </div>
                 </div>
@@ -91,7 +91,7 @@ this is third example keyword"></textarea>
             <div class="form-row text-left">
                 <div class="col">
                     <div class="form-label-group">
-                        <input type="text" class="form-control" id="video" name="video">
+                        <input type="text" class="form-control" id="video" name="video" placeholder="">
                         <label for="video">Link to Video</label>
                     </div>
                 </div>
@@ -102,7 +102,7 @@ this is third example keyword"></textarea>
             <input type="hidden" value="new" name="bot">
             @endif
             <div class="form-group py-4">
-               <button class="btn btn-lg btn-primary btn-block text-uppercase w-50 mx-auto" value="submit" type="submit">Start</button>
+               <button class="btn btn-lg btn-primary btn-block text-uppercase w-50 mx-auto" type="submit">Start</button>
             </div>
         </form>
     </div>
