ddcba126b8dcd14905a75b37d323885ba5ba18eb
zumbata <zumbataweee@live.com>
2020-03-06 20:47:25 +0200

    asd

--- server/resources/views/includes/header.blade.php
+++ server/resources/views/includes/header.blade.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">J & M Bot</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="/admin">Home</a>
+      <a class="nav-item nav-link" href="/admin">Home</a>
       @if(Session::has('isAdminLogged'))
       <a class="nav-item nav-link" href="/admin/logout">Logout</a>
       <a class="nav-item nav-link" href="/admin/log/bot">Bot Log</a>
