983e68352a317b39db99cc6e7f581ff7a2139de2
zumbata <zumbataweee@live.com>
2020-03-22 23:22:42 +0200

    last update

--- new_bot/bot.py
+++ new_bot/bot.py
@@ -54,36 +54,36 @@ for proxy in proxies:
 		continue
 	driver.set_page_load_timeout(20)
 	driver.implicitly_wait(20)
-	try:
-		driver.get('https://youtube.com/')
-	except Exception as e:
-		print(f"{datetime.now().strftime('%H:%M:%S')} : Exception occured in line {sys._getframe().f_lineno}")
-		print(str(e))
-		driver.quit()
-		continue
-	moveon = True
-	try:
-		search = driver.find_element(By.CSS_SELECTOR, 'input#search')
-		button = driver.find_element(By.CSS_SELECTOR, '#search-icon-legacy')
-		driver.execute_script(f'arguments[0].value = "{random.choice(keywords)}";', search)
-		driver.execute_script(f'arguments[0].click();', button)
-	except Exception as e:
-		moveon = False
-	flag = False
-	if moveon == True:
-		for x in range(1,20):
-			try:
-				link = driver.find_element(By.CSS_SELECTOR, 'ytd-video-renderer.ytd-item-section-renderer:nth-child({}) > div:nth-child(1) > ytd-thumbnail:nth-child(1) > a'.format(str(x)))
-			except Exception as e:
-				continue
-			driver.execute_script('arguments[0].scrollIntoView();', link)
-			href = link.get_attribute('href')
-			if href in site :
-				driver.execute_script(f'arguments[0].click();', link)
-				flag = True
-				break
-	if flag == False or moveon == False:
-		driver.get(site)
+	# try:
+	# 	driver.get('https://youtube.com/')
+	# except Exception as e:
+	# 	print(f"{datetime.now().strftime('%H:%M:%S')} : Exception occured in line {sys._getframe().f_lineno}")
+	# 	print(str(e))
+	# 	driver.quit()
+	# 	continue
+	# moveon = True
+	# try:
+	# 	search = driver.find_element(By.CSS_SELECTOR, 'input#search')
+	# 	button = driver.find_element(By.CSS_SELECTOR, '#search-icon-legacy')
+	# 	driver.execute_script(f'arguments[0].value = "{random.choice(keywords)}";', search)
+	# 	driver.execute_script(f'arguments[0].click();', button)
+	# except Exception as e:
+	# 	moveon = False
+	# flag = False
+	# if moveon == True:
+	# 	for x in range(1,20):
+	# 		try:
+	# 			link = driver.find_element(By.CSS_SELECTOR, 'ytd-video-renderer.ytd-item-section-renderer:nth-child({}) > div:nth-child(1) > ytd-thumbnail:nth-child(1) > a'.format(str(x)))
+	# 		except Exception as e:
+	# 			continue
+	# 		driver.execute_script('arguments[0].scrollIntoView();', link)
+	# 		href = link.get_attribute('href')
+	# 		if href in site :
+	# 			driver.execute_script(f'arguments[0].click();', link)
+	# 			flag = True
+	# 			break
+	# if flag == False or moveon == False:
+	driver.get(site)
 	moveon = True
 	try:
 		play = driver.find_element(By.CSS_SELECTOR, '.ytp-play-button')
