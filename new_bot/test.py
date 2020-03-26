b1ce0d2efa4282d7716019bcb698a986ee1451ff
Dimitar Kostov <dimitar.d.kostov@hotmail.com>
2020-03-26 20:53:57 +0200

    asdasdasd

--- new_bot/test.py
+++ new_bot/test.py
@@ -1,20 +1,23 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
 import sys
+
+
+options = Options()
+options.headless = True
+options.log.level = "trace"
 prox = sys.argv[1]
 ip = prox.split(':')[0]
 port = prox.split(':')[1]
+print(ip)
+print(port)
 profile = webdriver.FirefoxProfile()
 profile.set_preference("network.proxy.type", 1)
 profile.set_preference("network.proxy.socks", ip)
 profile.set_preference("network.proxy.socks_port", int(port))
 profile.set_preference("network.proxy.socks_version", 5)
 profile.update_preferences()
-driver = webdriver.Firefox(firefox_profile=profile)
-
+driver = webdriver.Firefox(firefox_profile=profile, options=options)
 driver.get("https://ipecho.net/plain")
 print(driver.page_source)
 driver.quit()
