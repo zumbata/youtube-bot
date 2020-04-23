57a04b5808161aedcd1716abaa1736977b00393e
Travis <travis@travis-ci.org>
2020-04-23 18:29:03 +0300

    try

--- new_bot/test.py
+++ new_bot/test.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.by import By
+
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from time import sleep
 
@@ -9,6 +11,7 @@ capa["pageLoadStrategy"] = "none"
 chrome_options = ChromeOptions()
 # chrome_options.add_argument('--proxy-server=socks5://' + proxy)
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
 chrome_options.add_experimental_option("useAutomationExtension", False)
 driver = webdriver.Chrome(desired_capabilities=capa, options=chrome_options)
@@ -19,10 +22,11 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
         })
     """
 })
-driver.implicitly_wait(100)
-
+driver.implicitly_wait(20)
 driver.get('https://www.youtube.com/watch?v=MxFoEVzxxb8')
-
+play = driver.find_element(By.CSS_SELECTOR, '.ytp-play-button')
+text = play.get_attribute('title')
+if (text.find('Play') != -1):
+    driver.execute_script('arguments[0].click();', play)
 sleep(30)
-
 driver.quit()
