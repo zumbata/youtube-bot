fa51926595e57d88e53af4994a12d78c71d6b831
Travis <travis@travis-ci.org>
2020-04-23 18:42:21 +0300

    test

--- new_bot/test.py
+++ new_bot/test.py
@@ -1,15 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
-
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from time import sleep
 
-# proxy = '209.250.237.162:35584'
+proxy = '209.250.237.162:35584'
 capa = DesiredCapabilities.CHROME
 capa["pageLoadStrategy"] = "none"
 chrome_options = ChromeOptions()
-# chrome_options.add_argument('--proxy-server=socks5://' + proxy)
+chrome_options.add_argument('--proxy-server=socks5://' + proxy)
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
@@ -23,10 +22,10 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
     """
 })
 driver.implicitly_wait(20)
-driver.get('https://www.youtube.com/watch?v=MxFoEVzxxb8')
-play = driver.find_element(By.CSS_SELECTOR, '.ytp-play-button')
-text = play.get_attribute('title')
-if (text.find('Play') != -1):
-    driver.execute_script('arguments[0].click();', play)
-sleep(30)
-driver.quit()
+for i in range(20):
+    driver.get('https://www.youtube.com/watch?v=MxFoEVzxxb8')
+    sleep(5)
+    print(driver.page_source)
+    play = driver.find_element(By.CSS_SELECTOR, 'body').send_keys('k')
+    sleep(30)
+    driver.quit()
