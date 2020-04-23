fee0eadeb7c387a8fb05c0dc456662dc9c33c273
Travis <travis@travis-ci.org>
2020-04-23 18:18:44 +0300

    test

--- new_bot/test.py
+++ new_bot/test.py
@@ -1,22 +1,28 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-import sys
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from time import sleep
 
+# proxy = '209.250.237.162:35584'
+capa = DesiredCapabilities.CHROME
+capa["pageLoadStrategy"] = "none"
+chrome_options = ChromeOptions()
+# chrome_options.add_argument('--proxy-server=socks5://' + proxy)
+chrome_options.add_argument("--headless")
+chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+chrome_options.add_experimental_option("useAutomationExtension", False)
+driver = webdriver.Chrome(desired_capabilities=capa, options=chrome_options)
+driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    "source": """
+        Object.defineProperty(navigator, 'webdriver', {
+            get: () => undefined
+        })
+    """
+})
+driver.implicitly_wait(100)
 
-options = Options()
-options.headless = True
-options.log.level = "trace"
-prox = sys.argv[1]
-# prox = '209.250.237.162:33361'
-ip = prox.split(':')[0]
-port = prox.split(':')[1]
-profile = webdriver.FirefoxProfile()
-profile.set_preference("network.proxy.type", 1)
-profile.set_preference("network.proxy.socks", ip)
-profile.set_preference("network.proxy.socks_port", int(port))
-profile.set_preference("network.proxy.socks_version", 5)
-profile.update_preferences()
-driver = webdriver.Firefox(firefox_profile=profile, options=options)
-driver.get("https://ipecho.net/plain")
-print(driver.page_source)
+driver.get('https://www.youtube.com/watch?v=MxFoEVzxxb8')
+
+sleep(30)
+
 driver.quit()
